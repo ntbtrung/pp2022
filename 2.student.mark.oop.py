140c93ff25b64a5e3864b640cc0aaa3f34e0e6ac
ntbtrung <85278958+ntbtrung@users.noreply.github.com>
2022-03-29 13:24:34 +0700

    Create 2.student.mark.oop.py

---
+++ 2.student.mark.oop.py
@@ -0,0 +1,30 @@
+
+class Student:
+    def __init__(self, name, ID, marks1, marks2):
+        self.name = name
+        self.ID = ID
+        self.marks1 = marks1
+        self.marks2 = marks2
+
+    def accept(self, Name, ID, marks1, marks2):
+        ob = Student(Name, ID, marks1, marks2)
+        ls.append(ob)
+
+    def display(self, ob):
+        print("Name : ", ob.name)
+        print("ID : ", ob.ID)
+        print("C course : ", ob.marks1)
+        print("Python course : ", ob.marks2)
+        print("\n")
+
+ls = []
+obj = Student('', 0, 0, 0)
+
+obj.accept("Paul", 1, 100, 100)
+obj.accept("Chris", 2, 90, 90)
+obj.accept("Jake", 3, 80, 80)
+
+print("\n")
+print("\nList of Students\n")
+for i in range(ls.__len__()):
+    obj.display(ls[i])
