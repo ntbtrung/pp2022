9db40926483fa17d0ed54b2cc6610d19c2eb7d90
ntbtrung <85278958+ntbtrung@users.noreply.github.com>
2022-04-06 23:35:37 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,81 +1,96 @@
-#nguyen trinh bao trung bi11-275 labwork 1
+'''
+nguyễn trịnh bảo trung bi11-275
+'''
+
+
 def inputStudentCount():
     count = int(input("Enter number of students: "))
-    return int(count)
+    return count
+
 
 def inputStudentInfo(studentCount):
     # returns a dict of students, id is the key, with info from keyboard
-    students = {}
+    students = []
 
-    #input info for a student: {id, name, dob}
+    # input info for a student: {id, name, dob}
     for i in range(0, studentCount):
         id = input("Enter student ID: ")
         name = input("Enter student name: ")
         dob = input("Enter studen DoB: ")
-        students[id] = {
+        student = {
+            "id": id,
             "name": name,
             "dob": dob,
-            "mark": {}
+            "marks": {}
         }
+        students.append(student)
 
     return students
+
 
 def inputCourseCount():
     count = int(input("Enter number of courses: "))
     return count
 
-def inputCourseInfo(courseCount):
-    #return a list of courses, with info from keyboard
-    courses = {}
 
-    #input info for a course: {id, name}
+def inputCourseInfo(courseCount):
+    # return a list of courses, with info from keyboard
+    courses = []
+
+    # input info for a course: {id, name}
     for i in range(0, courseCount):
         id = input("Enter course ID: ")
         name = input("Enter course name: ")
-        courses[id] = name
+        course = {
+            "id": id,
+            "name": name
+        }
+        courses.append(course)
 
     return courses
 
-def inputMark(coursesid, students):
-    print(f"Enter mark of the course {coursesid} for student: ")
-    for id in students:
-        mark = float(input(f"- Student {students[id]['name']}"))
-        students[id]['mark'][coursesid] = mark
+
+def inputMark(courseid, students):
+    print(f"Enter marks of the course {courseid} for students: ")
+    for student in students:
+        mark = float(input(f"- Student {student['name']}\n"))
+        student['marks'][courseid] = mark
+
 
 def listCourses(courses):
     print("\n All courses list")
-    for id in courses:
-        print(f"{id: <10} {courses[id]: <20}")
+    for course in courses:
+        print(f"{course['id']: <10} {course['name']: <20}")
+
 
 def selectCourse(courses):
     listCourses(courses)
-    coursesid = input("Enter course id from the above table: ")
-    return coursesid
+    courseid = input("Enter course id from the table above: ")
+    return courseid
+
 
 def listStudents(students):
     print("\n All students list")
-    for id in students:
-        print(f"{id:<10} {students[id]['name']: <20} {students[id]['dob']: <15}")
+    for student in students:
+        print(f"{student['id']:<10} {student['name']: <20} {student['dob']: <15}")
 
-def showMark(coursesid, students):
-    print(f"\n All marks for the course {courses[id]}")
-    for id in students:
-        print(f"{students[id]['name']: <20} {students[id]['mark'][coursesid]}")
 
-#enter student count and information
+def showMark(courseid, students):
+    print(f"\n All marks for the course {courseid}")
+    for student in students:
+        print(f"{student['name']: <20} {student['marks'][courseid]}")
+
+
 studentCount = inputStudentCount()
 students = inputStudentInfo(studentCount)
 listStudents(students)
 
-#enter course count and information
 courseCount = inputCourseCount()
 courses = inputCourseInfo(courseCount)
 listCourses(courses)
 
-#select course, input mark for students in the given courses
-coursesid = selectCourse(courses)
-inputMark(coursesid, students)
+courseid = selectCourse(courses)
+inputMark(courseid, students)
 
-#show marks for a given course
-coursesid = selectCourse(courses)
-showMark(coursesid, students)
+course = selectCourse(courses)
+showMark(courseid, students)
